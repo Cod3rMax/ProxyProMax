5cfde406f64a3655c4631906401fdc83ff51bd4d
Cod3rMax <dzxxxw@gmail.com>
2022-10-04 18:20:55 +0200

    Fixing small pagination on the main page

--- resources/views/components/pagination-tabs-component.blade.php
+++ resources/views/components/pagination-tabs-component.blade.php
@@ -3,22 +3,22 @@
         <div class="row">
             <div class="slider-slides-wrap">
                 <div class="slides-item slides-item-primary swiper-slide-active">
-                    <span class="slides-item-text">WordPress Hosting</span>
+                    <span class="slides-item-text">Proxy Servers</span>
                 </div>
                 <div class="slides-item slides-item-red">
-                    <span class="slides-item-text">Shared Hosting</span>
+                    <span class="slides-item-text">Proxy Checker</span>
                 </div>
                 <div class="slides-item slides-item-orange">
-                    <span class="slides-item-text">VPS Hosting</span>
+                    <span class="slides-item-text">Free Proxy List</span>
                 </div>
                 <div class="slides-item slides-item-yellow">
-                    <span class="slides-item-text">Dedicated Server</span>
+                    <span class="slides-item-text">Clean Proxy List</span>
                 </div>
                 <div class="slides-item slides-item-blue">
-                    <span class="slides-item-text">Cloud Hosting</span>
+                    <span class="slides-item-text">No logs</span>
                 </div>
                 <div class="slides-item slides-item-lime">
-                    <span class="slides-item-text">Domains</span>
+                    <span class="slides-item-text">24/7 Support</span>
                 </div>
             </div>
         </div>
