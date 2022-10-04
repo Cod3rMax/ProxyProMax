4c26ca606d404c79e2f428ba6bb36e85aeed99c2
Cod3rMax <dzxxxw@gmail.com>
2022-10-04 06:29:28 +0200

    Make component pagination tabs for the main page and integrate it

---
+++ resources/views/components/pagination-tabs-component.blade.php
@@ -0,0 +1,26 @@
+<div class="slider-slides">
+    <div class="container">
+        <div class="row">
+            <div class="slider-slides-wrap">
+                <div class="slides-item slides-item-primary swiper-slide-active">
+                    <span class="slides-item-text">WordPress Hosting</span>
+                </div>
+                <div class="slides-item slides-item-red">
+                    <span class="slides-item-text">Shared Hosting</span>
+                </div>
+                <div class="slides-item slides-item-orange">
+                    <span class="slides-item-text">VPS Hosting</span>
+                </div>
+                <div class="slides-item slides-item-yellow">
+                    <span class="slides-item-text">Dedicated Server</span>
+                </div>
+                <div class="slides-item slides-item-blue">
+                    <span class="slides-item-text">Cloud Hosting</span>
+                </div>
+                <div class="slides-item slides-item-lime">
+                    <span class="slides-item-text">Domains</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,6 +16,8 @@
 <!-- ... end Main Header -->
 
 
+
+
 <div class="main-content-wrapper">
 
 	<section class="crumina-module crumina-module-slider crumina-main-slider">
@@ -66,32 +68,9 @@
 
 			<!--Pagination tabs-->
 
-			<div class="slider-slides">
-				<div class="container">
-					<div class="row">
-						<div class="slider-slides-wrap">
-							<div class="slides-item slides-item-primary swiper-slide-active">
-								<span class="slides-item-text">WordPress Hosting</span>
-							</div>
-							<div class="slides-item slides-item-red">
-								<span class="slides-item-text">Shared Hosting</span>
-							</div>
-							<div class="slides-item slides-item-orange">
-								<span class="slides-item-text">VPS Hosting</span>
-							</div>
-							<div class="slides-item slides-item-yellow">
-								<span class="slides-item-text">Dedicated Server</span>
-							</div>
-							<div class="slides-item slides-item-blue">
-								<span class="slides-item-text">Cloud Hosting</span>
-							</div>
-							<div class="slides-item slides-item-lime">
-								<span class="slides-item-text">Domains</span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+<x-pagination-tabs-component></x-pagination-tabs-component>
+
+            <!--Pagination tabs ends-->
 
 		</div>
 
@@ -850,6 +829,8 @@
 	</section>
 
 </div>
+
+
 
 
 <!-- Footer -->
