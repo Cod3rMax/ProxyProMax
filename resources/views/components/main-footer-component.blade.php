c12d8e181eb2c076cbc4aed236fa25b02b528dd4
Cod3rMax <dzxxxw@gmail.com>
2022-10-06 05:43:07 +0200

    fixing footer social media icons links add asset

--- resources/views/components/main-footer-component.blade.php
+++ resources/views/components/main-footer-component.blade.php
@@ -136,7 +136,7 @@
 								<a href="#">
 
 
-									<img class="crumina-icon " loading="lazy" src="img/theme-content/social-icons/discord.png" alt="discord">
+									<img class="crumina-icon " loading="lazy" src="{{ asset('img/theme-content/social-icons/discord.png') }}" alt="discord">
 
 								</a>
 							</li>
@@ -144,7 +144,7 @@
 								<a href="#">
 
 
-									<img class="crumina-icon " loading="lazy" src="img/theme-content/social-icons/twitter.png" alt="twitter">
+									<img class="crumina-icon " loading="lazy" src="{{ asset('img/theme-content/social-icons/twitter.png') }}" alt="twitter">
 
 								</a>
 							</li>
@@ -152,7 +152,7 @@
 								<a href="#">
 
 
-									<img class="crumina-icon " loading="lazy" src="img/theme-content/social-icons/instagram.png" alt="instagram">
+									<img class="crumina-icon " loading="lazy" src="{{ asset('img/theme-content/social-icons/instagram.png') }}" alt="instagram">
 
 								</a>
 							</li>
@@ -160,7 +160,7 @@
 								<a href="#">
 
 
-									<img class="crumina-icon " loading="lazy" src="img/theme-content/social-icons/youtube.png" alt="youtube">
+									<img class="crumina-icon " loading="lazy" src="{{ asset('img/theme-content/social-icons/youtube.png') }}" alt="youtube">
 
 								</a>
 							</li>
