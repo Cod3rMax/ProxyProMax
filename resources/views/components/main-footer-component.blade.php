58cc9213cf5a971a744795c0749a1814b1772bda
Cod3rMax <dzxxxw@gmail.com>
2022-10-06 03:34:35 +0200

    Fixing the footer and put it in a component

---
+++ resources/views/components/main-footer-component.blade.php
@@ -0,0 +1,220 @@
+<footer id="site-footer" class="footer footer--dark footer--with-decoration">
+
+	<div class="footer-content">
+		<div class="container">
+			<div class="row justify-content-between">
+				<div class="col-lg-2 col-md-6 col-sm-12 col-xs-12 mb-4 mb-lg-0">
+					<div class="widget widget_links">
+						<h5 class="widget-title">
+							ProxyProMax
+						</h5>
+						<ul>
+							<li>
+								<a href="02_wp_host.html">
+									Home
+									<svg class="crumina-icon">
+										<use xlink:href="#icon-triangle-arrow"></use>
+									</svg>
+								</a>
+							</li>
+							<li>
+								<a href="03_shared_host.html">
+									Free Proxy List
+									<svg class="crumina-icon">
+										<use xlink:href="#icon-triangle-arrow"></use>
+									</svg>
+								</a>
+							</li>
+							<li>
+								<a href="04_vps_host.html">
+									UK Proxy List
+									<svg class="crumina-icon">
+										<use xlink:href="#icon-triangle-arrow"></use>
+									</svg>
+								</a>
+							</li>
+							<li>
+								<a href="05_dedicated_server.html">
+									USA Proxy List
+									<svg class="crumina-icon">
+										<use xlink:href="#icon-triangle-arrow"></use>
+									</svg>
+								</a>
+							</li>
+							<li>
+								<a href="06_cloud_hosting.html">
+									Clean Proxy List
+									<svg class="crumina-icon">
+										<use xlink:href="#icon-triangle-arrow"></use>
+									</svg>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-lg-2 col-md-6 col-sm-12 col-xs-12 mb-4 mb-lg-0">
+					<div class="widget widget_links">
+						<h5 class="widget-title">
+							Tools
+						</h5>
+						<ul>
+							<li>
+								<a href="07_domains.html">
+									Proxy Checker
+									<svg class="crumina-icon">
+										<use xlink:href="#icon-triangle-arrow"></use>
+									</svg>
+								</a>
+							</li>
+                            <li>
+								<a href="07_domains.html">
+									Server Status
+									<svg class="crumina-icon">
+										<use xlink:href="#icon-triangle-arrow"></use>
+									</svg>
+								</a>
+							</li>
+
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-lg-2 col-md-6 col-sm-12 col-xs-12 mb-4 mb-lg-0">
+					<div class="widget widget_links">
+						<h5 class="widget-title">
+							Support
+						</h5>
+						<ul>
+							<li>
+								<a href="09_about.html">
+									Terms & Conditions
+									<svg class="crumina-icon">
+										<use xlink:href="#icon-triangle-arrow"></use>
+									</svg>
+								</a>
+							</li>
+							<li>
+								<a href="10_knowledge_base.html">
+									Privacy Policy
+									<svg class="crumina-icon">
+										<use xlink:href="#icon-triangle-arrow"></use>
+									</svg>
+								</a>
+							</li>
+							<li>
+								<a href="15_blog.html">
+									FAQ
+									<svg class="crumina-icon">
+										<use xlink:href="#icon-triangle-arrow"></use>
+									</svg>
+								</a>
+							</li>
+							<li>
+								<a href="17_contacts.html">
+									Contacts US
+									<svg class="crumina-icon">
+										<use xlink:href="#icon-triangle-arrow"></use>
+									</svg>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-md-6 col-sm-12 col-xs-12 mb-0 mb-lg-0">
+					<div class="widget w-info">
+						<a href="index.html" class="site-logo">
+
+
+							<img loading="lazy" src="img/demo-content/logo-white.png" alt="logo" width="185">
+
+						</a>
+						<p>ProxyProMax offers the best checked proxies, for free & real human support.</p>
+						<ul class="socials">
+							<li>
+								<a href="#">
+
+
+									<img class="crumina-icon " loading="lazy" src="img/theme-content/social-icons/facebook.png" alt="facebook">
+
+								</a>
+							</li>
+							<li>
+								<a href="#">
+
+
+									<img class="crumina-icon " loading="lazy" src="img/theme-content/social-icons/twitter.png" alt="twitter">
+
+								</a>
+							</li>
+							<li>
+								<a href="#">
+
+
+									<img class="crumina-icon " loading="lazy" src="img/theme-content/social-icons/google.png" alt="google">
+
+								</a>
+							</li>
+							<li>
+								<a href="#">
+
+
+									<img class="crumina-icon " loading="lazy" src="img/theme-content/social-icons/youtube.png" alt="youtube">
+
+								</a>
+							</li>
+							<li>
+								<a href="#">
+
+
+									<img class="crumina-icon " loading="lazy" src="img/theme-content/social-icons/rss.png" alt="rss">
+
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="footer-subscribe-panel">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-3 col-md-4 col-sm-12 col-xs-12 mb-4 mb-lg-0">
+					<h5 class="footer-subscribe-panel-title text-white">SUBSCRIBE TO OUR NEWSLETTER</h5>
+				</div>
+				<div class="col-lg-3 col-md-4 col-sm-12 col-xs-12 mb-4 mb-lg-0">
+					<span class="footer-subscribe-panel-subtitle"><span class="font-weight-bold">Join Our Newsletter & Marketing Communication.</span> We'll send you news and offers.</span>
+				</div>
+				<div class="col-lg-6 col-md-4 col-sm-12 col-xs-12 mb-0 mb-lg-0">
+					<form class="footer-subscribe-form">
+						<div class="input-btn--inline">
+							<input class="input--dark" type="text" placeholder="Choose your new web address…">
+							<button type="button" class="crumina-button button--lime button--l">SEARCH</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="sub-footer bg-black">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center mb-0 mb-lg-0">
+					<div class="copyright">
+						<span>Copyright © 2020 <a href="#">ProxyProMax</a> Developed by <a href="https://crumina.net/">Cod3rMax</a></span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<a class="back-to-top" href="#">
+		<svg class="crumina-icon">
+			<use xlink:href="#icon-to-top"></use>
+		</svg>
+	</a>
+</footer>
