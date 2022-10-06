89b13238af565d37c97088eb78f79b43a556da23
Cod3rMax <dzxxxw@gmail.com>
2022-10-07 00:23:57 +0200

    Add newsletter design from blade to vue js #4

---
+++ resources/js/components/NewsLetterComponent.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="footer-subscribe-panel">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12 mb-4 mb-lg-0">
+                <h5 class="footer-subscribe-panel-title text-white">SUBSCRIBE TO OUR NEWSLETTER</h5>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12 mb-4 mb-lg-0">
+                <span class="footer-subscribe-panel-subtitle">
+                    <span class="font-weight-bold">Subscribe now and get free proxies list directly sent to your email.
+                    </span></span>
+            </div>
+            <div class="col-lg-6 col-md-4 col-sm-12 col-xs-12 mb-0 mb-lg-0">
+
+                    <div class="input-btn--inline">
+                        <input class="input--dark" type="text" placeholder="Enter your email">
+                        <button type="button" class="crumina-button button--lime button--l">Subscribe</button>
+                    </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+  methods:{
+
+
+  },
+
+  data(){
+        return {
+
+        }
+  }
+
+
+
+    }
+</script>
