0d02b06961db887f5100ae51da0732370c3b9672
Cod3rMax <dzxxxw@gmail.com>
2022-10-06 21:10:56 +0200

    Terms page is ready this should fixes #1

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>HOSTSight - About</title>
+
+    <link rel="shortcut icon" href="{{asset('img/favicon.ico')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/vendors/Bootstrap/bootstrap.min.css') }}">
+
+</head>
+<body>
+
+
+    <!-- Main Header -->
+
+    <x-main-navbar-component></x-main-navbar-component>
+
+    <!-- ... end Main Header -->
+
+
+
+
+
+<div class="main-content-wrapper">
+
+    <section class="crumina-stunning-header stunning-header-bg1 pb60">
+        <div class="container">
+            <div class="row justify-content-center align-center">
+                <div class="col-lg-8 col-md-10 col-sm-12 col-xs-12">
+                    <h1 class="page-title text-white">ProxyProMax Terms & Conditions</h1>
+                    <p class="page-text text-white">We take our terms & conditions seriously.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+	<div id="our-story" class="crumina-breadcrumbs breadcrumbs--dark-themes">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<ul class="breadcrumbs">
+						<li class="breadcrumbs-item">
+							<a href="index.html">Home</a>
+						</li>
+						<li class="breadcrumbs-item active">
+							<span>Terms & Conditions</span>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+    <section class="small-section-padding section-bg1">
+        <div class="container">
+            <div class="d-flex justify-content-center">
+                <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12 mb-4 mb-lg-0">
+                    <h2 class="align-center">ProxyProMax Terms</h2>
+                    <br>
+                </div>
+
+            </div>
+
+            <div style="font-size: large">
+                This is an agreement between you and <b>ProxyMaxPro</b> Proxy (“Service”) which is operated by <b>ProxyMaxPro</b> Network Ltd. (“us”, “we”, or “our”) for Proxy services,
+                related services and features. It is NOT a service for the commission of criminal acts. It is an anonymity and privacy service. You agree not to violate any laws
+                of any jurisdiction you are originating from, or terminating into.
+
+                <br><br>
+
+                You agree to protect your user id and access code in our system from unauthorized use and not to publish or share your access code with third parties.
+                You are responsible for all actions that occur on our servers where the login uses your user id/passcode.
+
+                <br><br>
+
+                You agree not to use the service system for sending spam, port scanning, scanning for open proxies or open relays,
+                sending opt-in email, unsolicited email or any type or version of email sent in large quantities even if the email is
+                ultimately sent off of another server. You agree not to attack in any way shape or form any other computer or network while on our service.
+
+                <br><br>
+
+                Hacking, cracking, distribution of viruses, fraudulent activities, network sabotage, phishing, and/or any conduct
+                deemed illegal or unwanted shall be subject to suspension or termination of user account on <b>PROXYPROMAX</b> network. Users are responsible for the proper
+                secure configuration of their services and are responsible for any damages caused by their neglect or exposure
+                of vulnerabilities whether intentional or unintentional.
+
+                <br><br>
+
+                <h1>MODIFICATIONS OF THE FREE SERVICES</h1>
+                <p>
+                    <b>PROXYPROMAX</b> reserves the right to modify or discontinue all or part of the Free Services, temporarily or permanently, with or without notice to Users, and is not obligated to support or update the Free Service.
+                    <br><br>
+                    The amended Terms shall be effective immediately after they are initially posted on the Site. Your continued use of the Free Services after the posting of the amended Terms on the Site constitutes your affirmative:
+                    <br><br>
+                    1*acknowledgment of the Terms and its modifications; and
+                    <br>
+                    2*agreement to abide and be bound by the Terms, as amended.
+                </p>
+                <p>
+                    You acknowledge and agree that <b>PROXYPROMAX</b> shall not be liable to you or any third party in the event that <b>PROXYPROMAX</b> Limited exercises its right to modify or discontinue all or part of the Free Services.
+                </p>
+
+                <br>
+
+                <h1>CHILDREN AND MINORS</h1>
+                <p>
+                    By using our Services, you affirm that you are at least 18 years of age, or otherwise over the age of majority in the
+                    jurisdiction in which you reside. You understand that the Internet may contain information which may be offensive, may be illegal in various jurisdictions or may be
+                    inappropriate for certain ages and that <b>PROXYPROMAX</b> Limited is not responsible for detection and removal of such information.
+                </p>
+
+                <h1>Changes</h1>
+                <p>
+                    This is the whole agreement. From time to time, we may post a new terms of service that will become binding and it is
+                    the responsibility of the users to periodically check for new Terms of Service.
+                </p>
+
+            </div>
+
+
+        </div>
+    </section>
+
+
+
+
+
+
+
+    <div class="crumina-breadcrumbs breadcrumbs--red">&nbsp;</div>
+
+    <x-main-faq-component></x-main-faq-component>
+
+
+    <div class="crumina-breadcrumbs breadcrumbs--red">&nbsp;</div>
+</div>
+
+
+<!-- Footer -->
+
+<x-main-footer-component :message="$SocialMediaLinks"></x-main-footer-component>
+
+<!-- ... end Footer -->
+
+
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+
+
+
+</body>
+</html>
