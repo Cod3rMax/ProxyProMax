4e07f1377b9beca277b6668711deebd6291f2e34
Cod3rMax <dzxxxw@gmail.com>
2022-10-06 18:44:16 +0200

    privacy policy page almost ready

--- resources/views/policytemplate.blade.php
+++ resources/views/policytemplate.blade.php
@@ -45,7 +45,7 @@
 							<a href="index.html">Home</a>
 						</li>
 						<li class="breadcrumbs-item active">
-							<span>About</span>
+							<span>Privacy Policy</span>
 						</li>
 					</ul>
 				</div>
@@ -53,246 +53,85 @@
 		</div>
 	</div>
 
-	<section class="large-section-padding bg-grey">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 align-center mb-5 ml-auto mr-auto">
-					<h2>SHORT STORY OUR COMPANY</h2>
-					<p class="fs-18 fw-medium">Quisque egestas diam in arcu cursus euismod. Lectus urna duis convallis convallis tellus. Sagittis orci a scelerisque purus semper eget.</p>
-				</div>
-			</div>
-			<div class="row tabs tabs--time-line align-items-center">
-				<div class="col-lg-2 col-md-12 col-sm-12 col-xs-12 mb-4 mb-lg-0">
-					<div class="nav nav-tabs" id="myTab" role="tablist">
+    <section class="small-section-padding section-bg1">
+        <div class="container">
+            <div class="d-flex justify-content-center">
+                <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12 mb-4 mb-lg-0">
+                    <h2 class="align-center">ProxyProMax Privacy</h2>
+                    <br>
+                </div>
 
-						<a class="nav-item nav-link active" id="time-2010-tab" data-toggle="tab" href="#time-2010" role="tab" aria-controls="time-2010" aria-selected="true">
-							2010
-							<div class="nav-item--point">
-								<div class="nav-item--arrow-wrap">
-									<svg class="crumina-icon">
-										<use xlink:href="#icon-arrow-right"></use>
-									</svg>
-								</div>
-							</div>
-						</a>
+            </div>
 
-						<a class="nav-item nav-link" id="time-2012-tab" data-toggle="tab" href="#time-2012" role="tab" aria-controls="time-2012" aria-selected="false">
-							2012
-							<div class="nav-item--point">
-								<div class="nav-item--arrow-wrap">
-									<svg class="crumina-icon">
-										<use xlink:href="#icon-arrow-right"></use>
-									</svg>
-								</div>
-							</div>
-						</a>
+            <div style="font-size: large">
+                The <b>ProxyProMax</b> service “PROXYPROMAX.COM” (“Service“) is operated by <b>PROXYPROMAX</b> Network Ltd. (“us“, “we“, or “our“) We value our customers’privacy and
+                believe that it is important for us to share the information about what kind of personal data we collect, process and store.
+
+                <br><br>
+                <h3>WHAT IS THE APPLICATION OF THIS POLICY?</h3>
+                <p>
+                    The application of this policy extends to our role as a controller with respect to the personal data of our
+                    site visitors and service users; in other words, where we establish the objectives and means of that personal data processing.
+                    <br><br>
+                    Regardless of our affiliation with any third-party service or product, this policy does not apply to such third-party
+                    affiliates. Our site contains hyperlinks to, and information about, third-party websites. We neither hold ourselves responsible for,
+                    nor do we have control over, these third parties’ privacy policies and practices.
+                    <br><br>
+                    You agree to our policy terms by using our website, products or services.
+                </p>
 
 
-						<a class="nav-item nav-link" id="time-2013-tab" data-toggle="tab" href="#time-2013" role="tab" aria-controls="time-2013" aria-selected="false">
-							2013
-							<div class="nav-item--point">
-								<div class="nav-item--arrow-wrap">
-									<svg class="crumina-icon">
-										<use xlink:href="#icon-arrow-right"></use>
-									</svg>
-								</div>
-							</div>
-						</a>
+                <h3>WHAT PERSONAL DATA GETS COLLECTED AND STORED WHEN YOU SIGNUP FOR OUR SERVICE?</h3>
+                <p>
+                    Because we want to maximize your privacy, we only collect the minimum amount of required information – your email address,
+                    which is stored in an encrypted mode on servers that are entirely encrypted.
+                    <br><br>
+                    Encryption technology will be used to protect any personal data, including your email address, that gets transferred from your web browser
+                    to our web server or the other way around.
+                    <br><br>
+                    We do not request or store your personal information, including your IP address or physical addresses.
+                    Personal Data within your account, such as your email address, may be used to reach out to you with marketing and promotional material,
+                    newsletters, and any other material that might interest you. You may use the unsubscribe link, instructions we provide in each email or
+                    contact us directly if you choose to opt out of receiving any, or all, of these communications from us. You agree to this procedure by
+                    signing up for a <b>PROXYPROMAX</b> account.
+                </p>
+
+                <h3>WHAT PERSONAL DATA GETS PROCESSED WHEN YOU USE OUR WEBSITE</h3>
+                <p>
+                    We may choose to process data regarding your use of our websites (“website usage data”). This data may include the information about the time, frequency and
+                    patterns of your visits, as well as your geographical location and IP address, operating system, browser type and version, referral source, pages viewed,
+                    length of visit, and website navigation paths. The web server logs are the source of the website usage data. This usage data processing is done in order
+                    to analyze the use of our website and services. It is legally based on our legitimate interest, such as monitoring,
+                    security, protecting and improving our website and its services.
+                </p>
+
+                <h3>ENFORCEMENT</h3>
+                <p>
+                    regularly reviews its compliance with this Policy. When we receive complaints, it is 's policy to contact the complaining user
+                    regarding his or her concerns. We will cooperate with the appropriate regulatory authorities, including local data
+                    protection authorities, to resolve any complaints regarding the transfer of personal data that cannot be resolved between and an individual.
+                </p>
+
+                <h3>CHANGES TO THIS POLICY</h3>
+                <p>
+                    Please note that this Privacy Policy may change from time to time. We will not reduce your rights under this Policy without your explicit consent,
+                    and we expect most such changes will be minor. Regardless, we will post any Policy changes on this page and, if the changes are significant,
+                    we will provide a more prominent notice (including, for certain services, email notification of Policy changes).
+                </p>
+
+            </div>
 
 
-						<a class="nav-item nav-link" id="time-2016-tab" data-toggle="tab" href="#time-2016" role="tab" aria-controls="time-2016" aria-selected="false">
-							2016
-							<div class="nav-item--point">
-								<div class="nav-item--arrow-wrap">
-									<svg class="crumina-icon">
-										<use xlink:href="#icon-arrow-right"></use>
-									</svg>
-								</div>
-							</div>
-						</a>
+        </div>
+    </section>
 
 
-						<a class="nav-item nav-link" id="time-2017-tab" data-toggle="tab" href="#time-2017" role="tab" aria-controls="time-2017" aria-selected="false">
-							2017
-							<div class="nav-item--point">
-								<div class="nav-item--arrow-wrap">
-									<svg class="crumina-icon">
-										<use xlink:href="#icon-arrow-right"></use>
-									</svg>
-								</div>
-							</div>
-						</a>
 
 
-						<a class="nav-item nav-link" id="time-2019-tab" data-toggle="tab" href="#time-2019" role="tab" aria-controls="time-2019" aria-selected="false">
-							2019
-							<div class="nav-item--point">
-								<div class="nav-item--arrow-wrap">
-									<svg class="crumina-icon">
-										<use xlink:href="#icon-arrow-right"></use>
-									</svg>
-								</div>
-							</div>
-						</a>
-
-					</div>
-				</div>
-				<div class="col-lg-10 col-md-12 col-sm-12 col-xs-12">
-					<div class="tab-content" id="myTabContent">
-						<div class="tab-pane fade show active" id="time-2010" role="tabpanel" aria-labelledby="time-2010-tab">
-							<div class="row align-items-center">
-								<div class="col-lg-5 col-md-6 col-sm-12 col-xs-12 mb-4 mb-lg-0">
-									<div class="tabs--time-line-thumb">
 
 
-										<img loading="lazy" src="img/demo-content/tabs-time-line/time-thumb.png" alt="icon">
+    <div class="crumina-breadcrumbs breadcrumbs--red">&nbsp;</div>
 
-									</div>
-								</div>
-								<div class="col-lg-7 col-md-6 col-sm-12 col-xs-12">
-									<div class="tabs--time-line-content">
-										<div class="tabs--time-line-subtitle">November, 2010</div>
-										<h3 class="tabs--time-line-title">Foundation of the Company</h3>
-										<p>
-											Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac turpis egestas integer eget aliquet nibh praesent tristique. Pharetra magna ac placerat vestibulum. Leo vel orci porta non pulvinar neque laoreet.
-										</p>
-										<p>
-											Ornare arcu dui vivamus arcu felis. Sed ullamcorper morbi tincidunt ornare massa eget egestas.
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						<div class="tab-pane fade" id="time-2012" role="tabpanel" aria-labelledby="time-2012-tab">
-							<div class="row align-items-center">
-								<div class="col-lg-5 col-md-6 col-sm-12 col-xs-12 mb-4 mb-lg-0">
-									<div class="tabs--time-line-thumb">
-
-
-										<img loading="lazy" src="img/demo-content/tabs-time-line/time-thumb.png" alt="icon">
-
-									</div>
-								</div>
-								<div class="col-lg-7 col-md-6 col-sm-12 col-xs-12">
-									<div class="tabs--time-line-content">
-										<div class="tabs--time-line-subtitle">November, 2012</div>
-										<h3 class="tabs--time-line-title">Foundation of the Company</h3>
-										<p>
-											Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac turpis egestas integer eget aliquet nibh praesent tristique. Pharetra magna ac placerat vestibulum. Leo vel orci porta non pulvinar neque laoreet.
-										</p>
-										<p>
-											Ornare arcu dui vivamus arcu felis. Sed ullamcorper morbi tincidunt ornare massa eget egestas.
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						<div class="tab-pane fade" id="time-2013" role="tabpanel" aria-labelledby="time-2013-tab">
-							<div class="row align-items-center">
-								<div class="col-lg-5 col-md-6 col-sm-12 col-xs-12 mb-4 mb-lg-0">
-									<div class="tabs--time-line-thumb">
-
-
-										<img loading="lazy" src="img/demo-content/tabs-time-line/time-thumb.png" alt="icon">
-
-									</div>
-								</div>
-								<div class="col-lg-7 col-md-6 col-sm-12 col-xs-12">
-									<div class="tabs--time-line-content">
-										<div class="tabs--time-line-subtitle">November, 2013</div>
-										<h3 class="tabs--time-line-title">Foundation of the Company</h3>
-										<p>
-											Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac turpis egestas integer eget aliquet nibh praesent tristique. Pharetra magna ac placerat vestibulum. Leo vel orci porta non pulvinar neque laoreet.
-										</p>
-										<p>
-											Ornare arcu dui vivamus arcu felis. Sed ullamcorper morbi tincidunt ornare massa eget egestas.
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						<div class="tab-pane fade" id="time-2016" role="tabpanel" aria-labelledby="time-2016-tab">
-							<div class="row align-items-center">
-								<div class="col-lg-5 col-md-6 col-sm-12 col-xs-12 mb-4 mb-lg-0">
-									<div class="tabs--time-line-thumb">
-
-
-										<img loading="lazy" src="img/demo-content/tabs-time-line/time-thumb.png" alt="icon">
-
-									</div>
-								</div>
-								<div class="col-lg-7 col-md-6 col-sm-12 col-xs-12">
-									<div class="tabs--time-line-content">
-										<div class="tabs--time-line-subtitle">November, 2016</div>
-										<h3 class="tabs--time-line-title">Foundation of the Company</h3>
-										<p>
-											Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac turpis egestas integer eget aliquet nibh praesent tristique. Pharetra magna ac placerat vestibulum. Leo vel orci porta non pulvinar neque laoreet.
-										</p>
-										<p>
-											Ornare arcu dui vivamus arcu felis. Sed ullamcorper morbi tincidunt ornare massa eget egestas.
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						<div class="tab-pane fade" id="time-2017" role="tabpanel" aria-labelledby="time-2017-tab">
-							<div class="row align-items-center">
-								<div class="col-lg-5 col-md-6 col-sm-12 col-xs-12 mb-4 mb-lg-0">
-									<div class="tabs--time-line-thumb">
-
-
-										<img loading="lazy" src="img/demo-content/tabs-time-line/time-thumb.png" alt="icon">
-
-									</div>
-								</div>
-								<div class="col-lg-7 col-md-6 col-sm-12 col-xs-12">
-									<div class="tabs--time-line-content">
-										<div class="tabs--time-line-subtitle">November, 2017</div>
-										<h3 class="tabs--time-line-title">Foundation of the Company</h3>
-										<p>
-											Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac turpis egestas integer eget aliquet nibh praesent tristique. Pharetra magna ac placerat vestibulum. Leo vel orci porta non pulvinar neque laoreet.
-										</p>
-										<p>
-											Ornare arcu dui vivamus arcu felis. Sed ullamcorper morbi tincidunt ornare massa eget egestas.
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						<div class="tab-pane" id="time-2019" role="tabpanel" aria-labelledby="time-2019-tab">
-							<div class="row align-items-center">
-								<div class="col-lg-5 col-md-6 col-sm-12 col-xs-12 mb-4 mb-lg-0">
-									<div class="tabs--time-line-thumb">
-
-
-										<img loading="lazy" src="img/demo-content/tabs-time-line/time-thumb.png" alt="icon">
-
-									</div>
-								</div>
-								<div class="col-lg-7 col-md-6 col-sm-12 col-xs-12">
-									<div class="tabs--time-line-content">
-										<div class="tabs--time-line-subtitle">November, 2019</div>
-										<h3 class="tabs--time-line-title">Foundation of the Company</h3>
-										<p>
-											Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac turpis egestas integer eget aliquet nibh praesent tristique. Pharetra magna ac placerat vestibulum. Leo vel orci porta non pulvinar neque laoreet.
-										</p>
-										<p>
-											Ornare arcu dui vivamus arcu felis. Sed ullamcorper morbi tincidunt ornare massa eget egestas.
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
 
 
 
