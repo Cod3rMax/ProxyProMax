702fdf0826c5762bdaced3236cae4ce43b9c806d
Cod3rMax <dzxxxw@gmail.com>
2022-10-06 03:38:24 +0200

    Fix the newsletter section and extract it to a component

--- resources/views/components/main-footer-component.blade.php
+++ resources/views/components/main-footer-component.blade.php
@@ -179,33 +179,14 @@
 		</div>
 	</div>
 
-	<div class="footer-subscribe-panel">
-		<div class="container">
-			<div class="row align-items-center">
-				<div class="col-lg-3 col-md-4 col-sm-12 col-xs-12 mb-4 mb-lg-0">
-					<h5 class="footer-subscribe-panel-title text-white">SUBSCRIBE TO OUR NEWSLETTER</h5>
-				</div>
-				<div class="col-lg-3 col-md-4 col-sm-12 col-xs-12 mb-4 mb-lg-0">
-					<span class="footer-subscribe-panel-subtitle"><span class="font-weight-bold">Join Our Newsletter & Marketing Communication.</span> We'll send you news and offers.</span>
-				</div>
-				<div class="col-lg-6 col-md-4 col-sm-12 col-xs-12 mb-0 mb-lg-0">
-					<form class="footer-subscribe-form">
-						<div class="input-btn--inline">
-							<input class="input--dark" type="text" placeholder="Choose your new web address…">
-							<button type="button" class="crumina-button button--lime button--l">SEARCH</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
+<x-main-newsletter-component></x-main-newsletter-component>
 
 	<div class="sub-footer bg-black">
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center mb-0 mb-lg-0">
 					<div class="copyright">
-						<span>Copyright © 2020 <a href="#">ProxyProMax</a> Developed by <a href="https://crumina.net/">Cod3rMax</a></span>
+						<span>Copyright © 2020 <a href="#">ProxyProMax</a> Developed by <a href="https://Cod3rMax.com">Cod3rMax</a></span>
 					</div>
 				</div>
 			</div>
