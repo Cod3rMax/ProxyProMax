64919240dd122df531542108ba620d2f8a59d691
Cod3rMax <dzxxxw@gmail.com>
2022-10-06 03:43:27 +0200

    Create Proxies model, factory, and migration

---
+++ database/factories/ProxiesFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Proxies>
+ */
+class ProxiesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'ProxyIP' => rand(100,200).".".rand(100,200).".".rand(50,200).".".rand(50,200).":".rand(1000,2000),
+            'Country' => $this->faker->country,
+            'Protocol' => $this->faker->randomElement(['HTTP','HTTPS','SOCKS5','SOCKS4','SOCKS4A']),
+            'Blacklisted' => rand(0,1),
+        ];
+    }
+}
