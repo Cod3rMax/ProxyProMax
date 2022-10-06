1f44a115d780ea5e3cbae7572bfe6aa1015e3a38
Cod3rMax <dzxxxw@gmail.com>
2022-10-06 18:26:48 +0200

    Fixing policy page navigation bar and make SocialMediaLinks global variable

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        view()->composer('*', function ($view) {
+            $view->with(["SocialMediaLinks"=>\App\Models\SocialMediaLink::all()]);
+        });
+
     }
 }
