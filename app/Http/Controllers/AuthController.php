7239debd09c0048caa7f6c891a6414b1c0cc8383
Cod3rMax <dzxxxw@gmail.com>
2022-10-09 00:03:11 +0200

    Registration system works this should fixes #3

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegistrationRequest;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -35,13 +36,28 @@ class AuthController extends Controller
     public function UserRegistration(RegistrationRequest $request){
 
 
-        return response()->json(
+        $user = User::create($request->merge(
+            [
+                'password'=>bcrypt($request->get('password')),
+                'password_confirmation'=>bcrypt($request->get('password_confirmation')),
+            ]
+            )->toArray());
+
+            if($user){
+                Auth::login($user);
+                return response()->json(['You have been registered!'], 200);
+            }
+
+            return response()->json(['message' => 'Can not register the user'], 422);
+
+
+/*         return response()->json(
             $request->merge(
                 [
                     'password'=>bcrypt($request->get('password')),
                     'password_confirmation'=>bcrypt($request->get('password_confirmation')),
                 ]
-        ), 200);
+        ), 200); */
 
     }
 
