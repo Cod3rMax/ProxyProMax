004eef9ff5ff5a407a1ebaa31b5647f31e672ce3
Cod3rMax <dzxxxw@gmail.com>
2022-10-09 01:06:10 +0200

    UserConfirmation Design ready

--- resources/js/components/Auth/UserConfirmationComponent.vue
+++ resources/js/components/Auth/UserConfirmationComponent.vue
@@ -1,59 +1,68 @@
 <template>
-    <section class="crumina-module crumina-module-slider crumina-main-slider">
-      <!-- Additional required wrapper -->
-      <div class="swiper-wrapper">
-        <!-- Slides -->
+  <section class="crumina-module crumina-module-slider crumina-main-slider">
+    <!-- Additional required wrapper -->
+    <div class="swiper-wrapper">
+      <!-- Slides -->
 
-        <div class="swiper-slide stunning-header-bg5">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 m-auto">
-                <a href="/">
-                  <img loading="lazy" src="/img/logo.png" alt="logo" />
-                </a>
-                <br /><br />
+      <div class="swiper-slide stunning-header-bg5">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 m-auto">
+              <a href="/">
+                <img loading="lazy" src="/img/logo.png" alt="logo" />
+              </a>
+              <br /><br />
 
+              <div
+                class="alert alert-danger alert-dismissible"
+              >
+                <a
+                  href="#"
+                  class="close"
+                  data-dismiss="alert"
+                  aria-label="close"
+                  >&times;</a
+                >
 
-                <div class="form-item">
-                  <input
-                    class="input--dark input--squared"
-                    type="text"
-                    placeholder="Enter your email"
-                  />
-                </div>
+                <strong>zefzef</strong>
+              </div>
 
+              <div class="form-item">
+                <input
+                  class="input--dark input--squared"
+                  type="text"
+                  placeholder="Enter your confirmation code...."
+                />
+              </div>
 
-
-                <div class="form-item">
-                  <div class="remember-wrapper text-white">
-                    <div class="checkbox">
-                      <label>Not registered? Create new account</label>
-                    </div>
+              <div class="form-item">
+                <div class="remember-wrapper text-white">
+                  <div class="checkbox">
+                    <label
+                      >Check your email we have sent you the code....</label
+                    >
                   </div>
                 </div>
-                <div class="form-item">
-                  <button
-                    type="button"
-                    class="crumina-button button--primary button--l w-100"
-                  >
-                    Sign In
-                  </button>
-                </div>
+              </div>
+              <div class="form-item">
+                <button
+                  type="button"
+                  class="crumina-button button--primary button--l w-100"
+                >
+                  Confirm account
+                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </section>
-  </template>
+    </div>
+  </section>
+</template>
 
 
 
     <script>
-
-  export default {
-
-
-  }
-  </script>
+export default {};
+</script>
 
