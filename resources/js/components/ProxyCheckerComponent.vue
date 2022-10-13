2d7cf0c0b580e740db644c784e7cc5179c2bed82
Cod3rMax <dzxxxw@gmail.com>
2022-10-13 05:37:15 +0200

    Proxy checker now format the proxy list and clean it from duplicate #6

--- resources/js/components/ProxyCheckerComponent.vue
+++ resources/js/components/ProxyCheckerComponent.vue
@@ -7,12 +7,10 @@
                     <div class="col-lg-8 col-md-10 col-sm-12 col-xs-12">
 
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <label for="descr" class="row justify-content-center font-weight-bold">Enter your Proxy List
-                                below:</label>
+                            <label for="descr" class="row justify-content-center font-weight-bold">Enter your Proxy List below:</label>
 
                             <div class="form-item">
-                                <textarea id="list" style="resize: none;" class="input--white input--squared"
-                                    placeholder="Enter your proxy list here" rows="11"></textarea>
+                                <textarea v-model="this.proxyList" @keyup="this.formatProxyList()" id="list" style="resize: none;" class="input--white input--squared" placeholder="Enter your proxy list here" rows="11"></textarea>
                             </div>
 
                             <div class="universal-btn-wrapper">
@@ -86,13 +84,37 @@
 export default {
     data(){
         return {
-
+            proxyList : [],
         }
     },
 
 
     methods: {
 
+        formatProxyList(){
+            this.proxyList = [];
+            var proxy_pattern = new RegExp('((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))(?![\\d])(?::|\\s+)(\\d+)', "gi");
+            var i = 0;
+            Array.prototype.getUnique = function () {
+                var u = {}, a = [];
+                for (var i = 0, l = this.length; i < l; ++i) {
+                    if (u.hasOwnProperty(this[i])) {
+                        continue;
+                    }
+                    a.push(this[i]);
+                    u[this[i]] = 1;
+                }
+                return a;
+            }
+            var inputlist = document.getElementById("list");
+            var proxies = inputlist.value;
+            var m = null;
+            while ((m = proxy_pattern.exec(proxies)) != null) {
+                this.proxyList.push(m[1] + ':' + m[2]);
+            }
+            this.proxyList = this.proxyList.getUnique();
+            this.proxyList = this.proxyList.join('\n');
+        },
 
     },
 
